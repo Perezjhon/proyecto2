e86c93e005d69c5fb839fa092fc8d15c32771b15
jhon Alexander <perezjhon908@gmail.com>
2024-09-09 11:34:10 -0500

    añadir fuentes de informacion

--- login_registro/usuarios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import registro, login, logout, home
-
-urlpatterns = [
-    path('registro/', registro, name='registro'),
-    path('login/', login, name='login'),
-    path('logout/', logout, name='logout'),
-    path('home/', home, name='home'),
-]
